6f4e25aa671f0063f7cb5c107daf4acdce4c10ac
spanero <s.panero.0449@vallauri.edu>
2020-04-16 12:05:16 +0200

    Lezione_16Apr
    Installato ed utilizzato il modulo npm json-server per creare API "fake" basate sui dati del file json creato da noi

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]=[
+    new Recipe('A Test Recipe','This is simply a test','https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg')
+  ];
 
   constructor() { }
 
