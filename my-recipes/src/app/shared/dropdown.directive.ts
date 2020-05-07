919d4754e0ed45c1961c1f4c121d1b55f5d0f54f
spanero <s.panero.0449@vallauri.edu>
2020-05-07 16:56:04 +0200

    Lezione 7/5/2020
    Aggiunta direttiva dropdown

---
+++ my-recipes/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener, HostBinding, ElementRef} from '@angular/core'
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+    
+    @HostBinding('class.open') isOpen: boolean = false;
+    
+    constructor(private elRef: ElementRef){}
+    //@HostListener('click') toggleOpen(){
+    //    this.isOpen = !this.isOpen;
+    //}
+
+    @HostListener('document:click',['$event']) toggleOpen(event: Event){
+        this.isOpen = this.elRef.nativeElement.contains(event.target)? !this.isOpen : false;
+    };
+}
